8cf643499b841aa3712992e4613743ec499b89ba
SeongUk <pkseonguk@gmail.com>
2021-02-17 03:16:51 +0000

    Study json

--- jsonst/jsonst.py
+++ jsonst/jsonst.py
@@ -40,17 +40,16 @@ with open("jsonst.json", "w") as json_file_w:
     json.dump(data_dic, json_file_w)
 # jason 파일로 출력
 
+json_str1 = json.dumps(data_dic)
+# 한줄로 생성
+json_str2 = json.dumps(data_dic, sort_keys=True)
+# 정렬
+json_str3 = json.dumps(data_dic, indent=4)
+# indent만큼 들여쓰기
+
 with open("jsonst.json", "r") as json_file_r:
     json_dic = json.load(json_file_r)
 # jason 파일 입력
 
 json_dic2 = json.loads(data_str)
 # jason 파일 입력2
-
-jason_str = json.dumps(data_dic)
-# 한줄로 생성
-jason_str3 = json.dumps(data_dic, sort_keys=True)
-# 정렬
-jason_str2 = json.dumps(data_dic, indent=4)
-# indent만큼 들여쓰기
-

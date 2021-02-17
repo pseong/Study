ee8ed3fbb448ad9e833407a5287adf359a9c2186
SeongUk <pkseonguk@gmail.com>
2021-02-17 02:30:06 +0000

    Study json

--- jsonst/jsonst.py
+++ jsonst/jsonst.py
@@ -13,8 +13,28 @@ data_dic = {
             "Age": 23,
             "List2": ["MyList1", "MyList2"]
         }
-    ]
-} 
+    ],
+    "false": False,
+    "null": None
+}
+
+data_str = '''{
+    "Name": "SeongUk",
+    "Age": 23, 
+    "List": [
+        {
+            "Name": "SeongUk2", 
+            "Age": 23
+        }, 
+        {
+            "Name": "SeongUk3",
+            "Age": 23,
+            "List2": ["MyList1", "MyList2"]
+        }
+    ],
+    "false": false,
+    "null": null
+}'''
 
 with open("jsonst.json", "w") as json_file_w:
     json.dump(data_dic, json_file_w)
@@ -24,10 +44,13 @@ with open("jsonst.json", "r") as json_file_r:
     json_dic = json.load(json_file_r)
 # jason 파일 입력
 
-jason_str = json.dumps(data_dic) # 한줄로 생성
-print(json_str)
-jason_str3 = json.dumps(data_dic, sort_keys=True) # 정렬
-print(json_str3)
-jason_str2 = json.dumps(data_dic, indent=4) # indent만큼 들여쓰기
-print(json_str2)
+json_dic2 = json.loads(data_str)
+# jason 파일 입력2
+
+jason_str = json.dumps(data_dic)
+# 한줄로 생성
+jason_str3 = json.dumps(data_dic, sort_keys=True)
+# 정렬
+jason_str2 = json.dumps(data_dic, indent=4)
+# indent만큼 들여쓰기
 

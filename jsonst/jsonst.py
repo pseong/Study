7ad21a3753f3791a33cdea53decc92db2f0e4fb5
SeongUk <pkseonguk@gmail.com>
2021-02-17 02:18:37 +0000

    Study jason

---
+++ jsonst/jsonst.py
@@ -0,0 +1,33 @@
+import json
+
+data_dic = {
+    "Name": "SeongUk",
+    "Age": 23, 
+    "List": [
+        {
+            "Name": "SeongUk2", 
+            "Age": 23
+        }, 
+        {
+            "Name": "SeongUk3",
+            "Age": 23,
+            "List2": ["MyList1", "MyList2"]
+        }
+    ]
+} 
+
+with open("jsonst.json", "w") as json_file_w:
+    json.dump(data_dic, json_file_w)
+# jason 파일로 출력
+
+with open("jsonst.json", "r") as json_file_r:
+    json_dic = json.load(json_file_r)
+# jason 파일 입력
+
+jason_str = json.dumps(data_dic) # 한줄로 생성
+print(json_str)
+jason_str3 = json.dumps(data_dic, sort_keys=True) # 정렬
+print(json_str3)
+jason_str2 = json.dumps(data_dic, indent=4) # indent만큼 들여쓰기
+print(json_str2)
+
